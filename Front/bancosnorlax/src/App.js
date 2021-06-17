66bc68a09077a9cd2ef7cfd038375ff449b1fe12
Robson <robsondesenvolvimento@gmail.com>
2021-06-17 09:21:42 -0300

    Update hooks

--- Front/bancosnorlax/src/App.js
+++ Front/bancosnorlax/src/App.js
@@ -1,31 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from "react";
-import { getAccountsFromApi } from './helpers/RequestsApi'
+import { GetAccountsFromApi } from './helpers/RequestsApi'
 
 function App() {
-  const [result, setResult] = useState(null);  
-    setResult(getAccountsFromApi());
-    return result
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>        
-      </header>
-      <p>Account: </p>
+        
+        <GetAccountsFromApi />       
+      </header>      
     </div>
   );
 }
--- Front/bancosnorlax/src/helpers/RequestsApi.js
+++ Front/bancosnorlax/src/helpers/RequestsApi.js
@@ -1,14 +1,46 @@
-const getAccountsFromApi = () => {
-  let arr = [];
+import React, { useState, useEffect } from 'react';
 
-  fetch("https://localhost:44301/api/Accounts")
-    .then((response) => response.json())
-    .then(resp => arr.push(...resp))
-    .catch((error) => {
-      console.error(error);
-    });
+function GetAccountsFromApi() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-    return arr
-};
+  // Nota: O array [] deps vazio significa
+  // este useEffect será executado uma vez
+  // semelhante ao componentDidMount()
+  useEffect(() => {
+    fetch("https://localhost:44301/api/accounts")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Nota: é importante lidar com errros aqui
+        // em vez de um bloco catch() para não receber
+        // exceções de erros reais nos componentes.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
 
-export { getAccountsFromApi };
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        {items.map(item => (
+          <p>
+            Id: {item.id} Agency: {item.agency} Account: {item.number} Sale: {item.sale}
+          </p>
+        ))}
+      </div>
+    );
+  }
+}
+
+export { GetAccountsFromApi };
